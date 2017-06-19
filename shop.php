5be21de602175ac334977d2d10ebfade6eebef00
NikolajHolm <niko4467@edu.campusvejle.dk>
2017-06-19 14:29:40 +0200

    Har lavet connect.php og insertArticle.php

--- shop.php
+++ shop.php
@@ -40,16 +40,17 @@
 			</nav>
 
 
+
+
+
+
 			<!-- Connect.php -->
 			<?php 
 			require "connect.php";
 			?>
 
-
 			<!-- sociale medier -->
-
-
-			<main class="col- col-sm-8 col-md-10">
+			<main class="col- col-sm-8 col-md-10 ">
 				<header class="">
 					<div class="row">
 						<div class="socialMedia d-flex">
@@ -78,6 +79,7 @@
 				<section class="noGutters">
 					<img class="img-fluid" src="img/banner.jpg" alt="">
 					<h5 class="text-center mt-5 mb-5">ØKOshop.DK - Økologiske og bæredygtigt fremsillede kvalitetsvarer - nemt og hurtigt</h5>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non hic ipsa laborum distinctio labore mollitia eligendi! Laborum omnis inventore praesentium provident corporis itaque, aliquam, repudiandae veniam tempore veritatis obcaecati asperiores!</p>
 				</section>
 
@@ -88,17 +90,20 @@
 
 
 				<section class="newProducts">	
-					<h6 class="text-center">Nyeste produkter</h6>
-					<hr>
+
 					<div class="container-fluid">
 						<div class="row">
 
-						<!-- Database artikler -->
+
+						<!-- Udskriv artiklerne fra databasen -->
 						<?php require "insertArticle.php" ?>
+
 							<!-- <article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
 								<img class="img-fluid" src="img/1.jpg" alt="">
 								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
 								<div class="text-center">
 									<button class="btn btn-green text-center buttonBuy" type="submit">KØB</button>
 								</div>
@@ -106,15 +111,62 @@
 							<article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
 								<img class=" img-fluid" src="img/2.jpg" alt="">
 								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
 								<div class="text-center">
-									<button class="btn btn-primary mb-4 buttonBuy" type="submit">KØB</button>
+									<button class="btn btn-primary mb-3 buttonBuy" type="submit">KØB</button>
 								</div>
 							</article>
 							<article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
 								<img class="img-fluid" src="img/3.jpg" alt="">
 								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
+								<div class="text-center">
+									<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
+								</div>
+							</article>
+
+
+							<article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
+								<img class="img-fluid" src="img/4.jpg" alt="">
+								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
+								<div class="text-center">
+									<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
+								</div>
+							</article>
+							<article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
+								<img class="img-fluid" src="img/5.jpg" alt="">
+								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
+								<div class="text-center">
+									<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
+								</div>
+							</article>
+							<article class="col-xs-3 col-sm-6 col-md-4 newArticle noGutters">
+								<img class="img-fluid" src="img/6.jpg" alt="">
+								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<div class="text-center">
+									<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
+								</div>
+							</article>
+
+
+							<article class="col-xs-3 col-sm-6 col-md-12 newArticle noGutters">
+								<img class="img-fluid mx-auto d-block" src="img/7.jpg" alt="">
+								<h3 class="mb-2">Produkt navn</h3>
+								<hr>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+								<p class="jumbotron text-center">15 kr.</p>
 								<div class="text-center">
 									<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
 								</div>
