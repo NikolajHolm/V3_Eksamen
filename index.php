3030e34de2ea6d71a06e46b9805a202348ea0ec6
NikolajHolm <niko4467@edu.campusvejle.dk>
2017-06-19 11:29:48 +0200

    Rettet nogle positioner, og arbejder med nyeste articles

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	<!-- Font Awesome -->
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- CSS -->
+
+	<link rel="stylesheet" href="css/styles.css">
+	<meta charset="UTF-8">
+	<title>Økoshop.DK</title>
+</head>
+<body>
+	<div class="container">
+
+		<!-- Nav med billede og links -->
+
+		<div class="row">
+			<nav class="col-md-2">
+				<img class="img-fluid" src="img/logo2.jpg" alt="">
+				<h4 class="text-center">
+					<span class="oeko">ØKO</span>shop<span class="dk">.DK</span>
+				</h4>
+				<section class="text-center d-flex flex-column">
+					<a href="#">Forside</a>
+					<a href="#">Shop</a>
+					<a href="#">Garanti</a>
+					<a href="#">Om os</a>
+					<a href="#">Log ind</a>
+				</section>
+			</nav>
+
+
+
+			<!-- sociale medier -->
+
+
+			<main class="col-md-10 ">
+				<header class="">
+					<div class="row">
+						<div class="socialMedia d-flex">
+							<div class="row justify-content-start">
+								<a class="ml-4" href="#"><i class="fa fa-facebook facebook" aria-hidden="true"></i></a>
+								<a href="#"><i class="fa fa-instagram instagram" aria-hidden="true"></i></a>
+								<a href="#"><i class="fa fa-twitter twitter" aria-hidden="true"></i></a>
+								<a href="#"><i class="fa fa-youtube youtube" aria-hidden="true"></i></a>
+							</div>
+						</div>
+						<div class="shopCart d-flex nav navbar-nav navbar-right">
+							<div class="row justify-content-end">
+								<a href="#">Min konto</a>
+								<i class="fa fa-shopping-cart shoppingCart ml-2 mr-2" aria-hidden="true"></i>
+								<a class="mr-3" href="#">Min kurv</a>
+							</div>
+						</div>
+					</div>
+				</header>
+
+
+
+
+				<!-- content -->
+				
+				<section class="noGutters">
+					<img class="img-fluid" src="img/banner.jpg" alt="">
+					<h5 class="text-center mt-5 mb-5">ØKOshop.DK - Økologiske og bæredygtigt fremsillede kvalitetsvarer - nemt og hurtigt</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non hic ipsa laborum distinctio labore mollitia eligendi! Laborum omnis inventore praesentium provident corporis itaque, aliquam, repudiandae veniam tempore veritatis obcaecati asperiores!</p>
+				</section>
+
+
+
+
+				<!-- Nye produkter -->
+
+
+				<section class="newProducts">	
+					<h6 class="text-center">Nyeste produkter</h6>
+					<hr>
+					<div class="row">
+						<article class="col-sm-6 col-md-4 newArticle noGutters">
+							<img class="img-fluid" src="img/1.jpg" alt="">
+							<h3>Produkt navn</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+							<div class="text-center">
+								<button class="btn btn-green text-center buttonBuy" type="submit">KØB</button>
+							</div>
+						</article>
+						<article class="col-sm-6 col-md-4 newArticle noGutters">
+							<img class=" img-fluid" src="img/2.jpg" alt="">
+							<h3>Produkt navn</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+							<div class="text-center">
+								<button class="btn btn-primary mb-4 buttonBuy" type="submit">KØB</button>
+							</div>
+						</article>
+						<article class="col-sm-6 col-md-4 newArticle noGutters">
+							<img class="img-fluid" src="img/3.jpg" alt="">
+							<h3>Produkt navn</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sequi, magnam rem, possimus expedita saepe non voluptatum quae vitae natus debitis! Perferendis alias earum pariatur repellat eaque totam in a.</p>
+							<div class="text-center">
+								<button class="btn btn-primary mb-4  buttonBuy" type="submit">KØB</button>
+							</div>
+						</article>
+					</div>	
+				</section>
+			</main>
+		</div>
+
+
+
+
+		<!-- Footer -->
+
+		<footer class="mt-5 col-md-12">
+			<div class="row">
+				<!-- Første link row -->
+				<section class="col-md-4 d-flex flex-column p-0">
+					<ul>
+						<li><a href="#">Kundeservice</a></li>
+						<li><a href="#">Handelsbetingelser</a></li>
+						<li><a href="#">Frisk frugt</a></li>
+						<li><a href="#">FAQ</a></li>
+					</ul>
+				</section>
+
+
+				<!-- Andet link row -->
+				<section class="col-md-4 d-flex flex-column">
+					<ul>
+						<li><a href="#">Presse</a></li>
+						<li><a href="#">Kontakt</a></li>
+						<li><a href="#">Følg os:</a></li>
+					</ul>
+				
+					<!-- Social icons -->
+
+	
+						<div class=" footerMedia ">
+							<a class="facebook" href="#"><i class="fa fa-facebook mr-2 mt-1" aria-hidden="true"></i></a>
+							<a class="instagram" href="#"><i class="fa fa-instagram mr-2 mt-1" aria-hidden="true"></i></a>
+							<a class="twitter" href="#"><i class="fa fa-twitter mr-2 mt-1" aria-hidden="true"></i></a>
+							<a class="youtube text-center" href="#"><i class="fa fa-youtube mr-2 mt-1" aria-hidden="true"></i></a>
+						</div>
+				</section>
+
+
+
+
+				<!-- Form i footer -->
+
+
+				<section class="col-md-4 text-center d-flex flex-column newsLetter">
+					<h4>Nyhedsbrev</h4>
+					<input class="mb-2" type="text" placeholder=" Fornavn">
+					<input class="mb-2" type="text" placeholder=" Efternavn">
+					<input class="mb-2" type="email" placeholder=" Email">
+					<button class="btn subscribe mb-5" type="submit">Tilmeld!</button>
+				</section>
+
+			</footer>
+		</div>
+	</body>
+	</html>
